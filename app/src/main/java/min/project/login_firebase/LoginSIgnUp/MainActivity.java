506f3a16d9762d1d80cbbe5b16e5ca7678309212
Shelton Fernandes <brucef1566@gmail.com>
2022-05-11 10:55:19 +0530

    new trial

---
+++ app/src/main/java/min/project/login_firebase/LoginSIgnUp/MainActivity.java
@@ -0,0 +1,16 @@
+package min.project.login_firebase.LoginSIgnUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import min.project.login_firebase.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
